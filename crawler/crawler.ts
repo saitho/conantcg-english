2ce337338b5b9f3974548f32ed5584f82e4d8d5b
Mario Lubenka <me@saitho.me>
2024-04-10 21:58:43 +0200

    chore(crawler): generate data for website

--- crawler/crawler.ts
+++ crawler/crawler.ts
@@ -3,15 +3,22 @@ import * as fs from "fs";
 import {Readable} from "stream";
 import {finished} from 'stream/promises';
 
-const url = 'https://www.takaratomy.co.jp/products/conan-cardgame/cardlist';
+const url = 'https://www.takaratomy.co.jp/products/conan-cardgame/cardlist'
 
 const result = await fetchHtmlDom(url)
+const cards = [];
 for (const cardImage of result.querySelectorAll('#cardList img')) {
-    const res = await fetch(cardImage.getAttribute('src'))
-
     const data = JSON.parse(cardImage.getAttribute('data') || '')
-    fs.writeFileSync('../cards/' + data.id + '.ja.json', JSON.stringify(data, null, 4))
+    cards.push(data)
 
-    const fileStream = fs.createWriteStream('../cards/' + data.id + '.ja.jpg', { flags: 'w' });
-    await finished(Readable.fromWeb(res.body).pipe(fileStream))
+    const imagePath = '../website/assets/cards/' + data.id + '.ja.jpg'
+    if (!fs.existsSync(imagePath)) {
+        const res = await fetch(cardImage.getAttribute('src'))
+        const fileStream = fs.createWriteStream(imagePath, { flags: 'w' })
+        await finished(Readable.fromWeb(res.body).pipe(fileStream))
+    }
 }
+
+const targetPath = '../website/data/cards_ja.json'
+const sortedCards = cards.sort((a, b) => Number(a.card_num) < Number(b.card_num) ? -1 : 1)
+fs.writeFileSync(targetPath, JSON.stringify(sortedCards))
