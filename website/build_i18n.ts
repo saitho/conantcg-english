acac120ea6e3f134b87dfc580ace15dd6b60240c
Mario Lubenka <me@saitho.me>
2024-04-13 14:05:11 +0200

    feat: implement translations

---
+++ website/build_i18n.ts
@@ -0,0 +1,16 @@
+// This script combines all localization files into one i18n file for Hugo
+
+import * as fs from "fs";
+import * as path from "path";
+
+const targetPath = __dirname + '/../website/i18n/en.json'
+const sourceDir = __dirname + '/../cards/translations/';
+const fullFile = {}
+for (const file of fs.readdirSync(sourceDir)) {
+    if (!file.endsWith('.en.json')) {
+        continue
+    }
+    const fileContent = JSON.parse(fs.readFileSync(path.join(sourceDir, file)).toString())
+    Object.assign(fullFile, fileContent)
+}
+fs.writeFileSync(targetPath, JSON.stringify(fullFile))
